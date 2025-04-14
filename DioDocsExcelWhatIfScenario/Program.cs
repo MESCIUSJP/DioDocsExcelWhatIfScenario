32d448ac6baec0e080f5b5e84349313d5cce33a8
Kuni <kuni.ujiie@mescius.com>
2025-04-14 09:42:59 +0900

    Updated.

--- DioDocsExcelWhatIfScenario/Program.cs
+++ DioDocsExcelWhatIfScenario/Program.cs
@@ -9,24 +9,15 @@ IWorksheet worksheet = workbook.ActiveSheet;
 
 // 値引き率が低いシナリオ
 List<object> lessDiscountRatesValues = [0.08, 0.05, 0.05, 0.03, 0.03];
-IScenario lessDiscountRates = worksheet.Scenarios.Add(
-    "低値引き率",
-    worksheet.Range["D2:D6"],
-    lessDiscountRatesValues);
+worksheet.Scenarios.Add("低値引き率", worksheet.Range["D2:D6"], lessDiscountRatesValues);
 
 // 標準の値引き率のシナリオ
 List<object> normalDiscountRatesValues = [0.1, 0.07, 0.07, 0.05, 0.05];
-IScenario normalDiscountRates = worksheet.Scenarios.Add(
-    "標準値引き率",
-    worksheet.Range["D2:D6"],
-    normalDiscountRatesValues);
+worksheet.Scenarios.Add("標準値引き率", worksheet.Range["D2:D6"], normalDiscountRatesValues);
 
 // 割引なしで販売するシナリオ
 List<object> sellingWithoutDiscountValues = [0, 0, 0, 0, 0];
-IScenario sellingWithoutDiscount = worksheet.Scenarios.Add(
-    "割引なし",
-    worksheet.Range["D2:D6"],
-    sellingWithoutDiscountValues);
+worksheet.Scenarios.Add("割引なし", worksheet.Range["D2:D6"], sellingWithoutDiscountValues);
 
 // 割引なしで販売するシナリオを表示
 worksheet.Scenarios["割引なし"].Show();
